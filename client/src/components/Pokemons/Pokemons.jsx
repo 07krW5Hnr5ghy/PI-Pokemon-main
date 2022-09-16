f000f48ab8cc9c49759d1eb66beceda100de5709
Juan Pablo Romero <jryn145@gmail.com>
2022-09-16 19:52:30 -0400

    filter name url get /pokemons first implement react

--- client/src/components/Pokemons/Pokemons.jsx
+++ client/src/components/Pokemons/Pokemons.jsx
@@ -1,23 +1,35 @@
 import { useEffect, useState, useMemo } from "react";
 import { useDispatch, useSelector } from "react-redux";
-import { getPokemons } from "../../redux/actions"; 
+import { getPokemons } from "../../redux/actions";
+import { useSearchParams } from "react-router-dom";
 import Page from "../Page/Page";
 import Loading from "../Loading/Loading";
 import Pokemon from "../Pokemon/Pokemon";
 import "./Pokemons.css";
+import { useLocation, useParams } from "react-router-dom";
 /* pokemons per page */
 let PageSize = 2;
 
 const Pokemons = () => {
+    const [searchParams] = useSearchParams();
     const dispatch = useDispatch();
     /* state tracking number of page */
     const [currentPage,setCurrentPage] = useState(1);
+    
+    /* request pokemons from db */
+    const name = searchParams.get('name');
+    console.log(name);
+    
+    useEffect(() => {
+        if(name){
+            dispatch(getPokemons(name));
+        }else{
+            dispatch(getPokemons());
+        }
+    },[dispatch]);
+
     /* select pokemons getted from db */
     let pokemons =  useSelector(state => state.reducerPokemon.pokemons);
-    /* request pokemons from db */
-    useEffect(() => {
-        dispatch(getPokemons());
-    },[dispatch]);
 
     /* set first and last page index
     divide the number of pokemons per page
--- client/src/redux/actions/index.js
+++ client/src/redux/actions/index.js
@@ -1,14 +1,24 @@
 import axios from "axios";
 export const GET_POKEMONS = "GET_POKEMONS";
 export const GET_DETAIL = "GET_DETAIL";
+export const GET_NAME = "GET_NAME";
 
-export const getPokemons = () => {
+export const getPokemons = (name) => {
     return async (dispatch) => {
-        const request = await axios('http://localhost:3001/pokemons');
-        return dispatch({
-            type:GET_POKEMONS,
-            payload:request.data,
-        });
+        if(name){
+            const request = await axios(`http://localhost:3001/pokemons?name=${name}`);
+            return dispatch({
+                type:GET_POKEMONS,
+                payload:request.data,
+            });
+        }else{
+            const request = await axios('http://localhost:3001/pokemons');
+            return dispatch({
+                type:GET_POKEMONS,
+                payload:request.data,
+            });
+        }
+        
     }
 }
 
