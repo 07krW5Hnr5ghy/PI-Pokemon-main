29bff8953dfcd6c3029165d8bf0e1cb6ddfc4163
Juan Pablo Romero <jryn145@gmail.com>
2022-09-20 00:04:52 -0400

    set background image Landing page

--- client/src/components/Landing/Landing.jsx
+++ client/src/components/Landing/Landing.jsx
@@ -1,11 +1,12 @@
-import "./Landing.css";
+import "./Landing.module.css";
 import {Link} from "react-router-dom";
 
 const Landing = () => {
     return(
         <>
-            <h1>Landing</h1>
-            <button><Link to={'/pokemons'}>Enter</Link></button>
+            <div>
+                <button><Link to={'/pokemons'}>Enter</Link></button>
+            </div>
         </>
     );
 };
