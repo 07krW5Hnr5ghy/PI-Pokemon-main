6000f2aebaed0cd498ee213e03da75f50f4d0fb5
Juan Pablo Romero <jryn145@gmail.com>
2022-12-01 12:20:24 -0500

    typescript config file

--- api/src/db.js
+++ api/src/db.js
@@ -6,6 +6,8 @@ const {
   DB_USER, DB_PASSWORD, DB_HOST,DB_NAME
 } = process.env;
 
+console.log(DB_USER);
+
 let sequelize = process.env.NODE_ENV === "production" ? new Sequelize({
   database:DB_NAME,
   dialect:"postgres",
