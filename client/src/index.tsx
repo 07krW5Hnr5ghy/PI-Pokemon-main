287cdc3385398d1c3f26b8cddedf6eb6a151bdef
Juan Pablo Romero <jryn145@gmail.com>
2022-12-03 22:31:59 -0500

    index file typescript

--- client/src/index.tsx
+++ client/src/index.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import {Provider} from 'react-redux';
 import store from "./redux/store/index";
 import './styles/styles.scss';
@@ -10,16 +10,18 @@ import axios from 'axios';
 
 axios.defaults.baseURL = process.env.REACT_APP_API || "http://localhost:3001";
 
-ReactDOM.render(
+const root = ReactDOM.createRoot(
+  document.getElementById('root') as HTMLElement
+);
+
+root.render(
   <Provider store={store}>
     <BrowserRouter>
       <React.StrictMode>
-        <App />
+        <App/>
       </React.StrictMode>
     </BrowserRouter>
   </Provider>
-  ,
-  document.getElementById('root')
 );
 
 // If you want to start measuring performance in your app, pass a function
