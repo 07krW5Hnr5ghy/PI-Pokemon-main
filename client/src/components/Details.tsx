3c6975e8f13a5760bf79a70a113270e376b5e581
Juan Pablo Romero <jryn145@gmail.com>
2023-01-08 23:50:11 -0500

    functional radar stats graphic in detail with mock data

--- client/src/components/Details.tsx
+++ client/src/components/Details.tsx
@@ -1,26 +1,52 @@
-import { useEffect } from "react";
+import { useEffect,useRef } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useParams,Link } from "react-router-dom";
 import {RootState} from "../redux/store";
 import { getDetail, resetDetail } from "../redux/pokemonActions";
 import Loading from "./Loading";
+import Chart from "./Chart";
 
 const Detail = () => {
     const {id} = useParams();
     const dispatch = useDispatch();
-
     const {detail} = useSelector((state:RootState) => state.pokemons);
 
     useEffect(() => {
-        if(id && !Object.keys(detail).length){
+        if(detail.id && detail.id !== id){
+            dispatch(resetDetail());
+        }
+        if(id && !detail.name){
             dispatch(getDetail(id));
         }
     },[dispatch,id,detail]);
+    console.log(detail);
 
+    const info = [100,200,300,400];
     
-
     return(
         <div>
+             {!Object.keys(detail).length ? 
+             <Loading/> :
+             <div className="Detail_container">
+                 <div className="Basic_info">
+                     <h2>{detail.name}</h2>
+                     <span>{detail.id}</span>
+                     <img src={detail.img}/>
+                 </div>
+                 <div className="Stats">
+                    <div className="Chart">
+                        <Chart/>
+                    </div>
+                 </div>
+             </div>
+             }
+         </div>
+    );
+}
+
+export default Detail;
+
+{/* <div>
             {!Object.keys(detail).length ? <Loading/> :
             <div id="Details_container">
                 <h2>{`< ${detail.name.charAt(0).toUpperCase() + detail.name.slice(1)} >`}</h2>
@@ -41,8 +67,4 @@ const Detail = () => {
                 </Link>
             </div>
             }
-        </div>
-    );
-}
-
-export default Detail;
+        </div> */}
