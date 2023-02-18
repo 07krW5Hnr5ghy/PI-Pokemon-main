b1670a26159ce9d3e3b75b742b42d97f799ba957
Juan Pablo Romero <jryn145@gmail.com>
2023-02-18 13:35:47 -0500

    refactor(database config):add database port enviroment variable

--- api/src/config/database.ts
+++ api/src/config/database.ts
@@ -4,14 +4,14 @@ import { DataSource } from "typeorm";
 import {Pokemon} from "../entity/Pokemon";
 import {Type} from "../entity/Type";
 const {
-    DB_USER, DB_PASSWORD, DB_HOST,DB_NAME
+    DB_USER, DB_PASSWORD, DB_HOST,DB_NAME,DB_PORT
 } = process.env;
 
 // config of postgres database
 export default new DataSource({
     type:"postgres",
     host:DB_HOST,
-    port: 5432,
+    port: Number(DB_PORT) || 5432,
     username: DB_USER,
     database: DB_NAME,
     password: DB_PASSWORD,
