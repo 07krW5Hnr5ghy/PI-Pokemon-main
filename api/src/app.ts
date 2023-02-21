3c4e46bbdf783928ce1bac04be2ff5f06cf6d5b4
Juan Pablo Romero <jryn145@gmail.com>
2023-02-21 10:43:07 -0500

    fix(backend cors policy):modify allow origin header to work with production enviroment frontend

--- api/src/app.ts
+++ api/src/app.ts
@@ -16,7 +16,7 @@ server.use(morgan('dev'));
 
 /* http request headers configuration */
 server.use((req, res, next) => {
-  res.header('Access-Control-Allow-Origin', process.env.FRONTEND || 'http://localhost:3000'); // update to match the domain you will make the request from
+  res.header('Access-Control-Allow-Origin', `${process.env.FRONTEND || 'http://localhost:3000'}`); // update to match the domain you will make the request from
   res.header('Access-Control-Allow-Credentials', 'true');
   res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
   res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, DELETE');
