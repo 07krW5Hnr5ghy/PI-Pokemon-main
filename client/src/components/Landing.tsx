baf27f70e7437039fd883481ecbfef236c6f37e9
Juan Pablo Romero <jryn145@gmail.com>
2023-02-21 12:22:24 -0500

    refactor(landing component):check redux state in landing component

--- client/src/components/Landing.tsx
+++ client/src/components/Landing.tsx
@@ -1,16 +1,29 @@
 import {Link} from "react-router-dom";
 import { PlayArrow } from "@mui/icons-material";
 import { useEffect } from "react";
-import { useDispatch } from "react-redux";
-import { getAPIData } from "../redux/pokemonActions";
+import { useDispatch,useSelector } from "react-redux";
+import { getAPIData,getDBPokemons,getTypes } from "../redux/pokemonActions";
+import { RootState } from "../redux/store";
+
 
 const Landing = () => {
 
     const dispatch = useDispatch();
+    /* selector of pokemon,types,filters and status data in redux state */
+    const {data,types} = useSelector((state:RootState) => state.pokemons);
+
     /* request to load remote api data */
     useEffect(() => {
         dispatch(getAPIData);
-    },[dispatch]);
+
+        if(!data.records.length){
+            dispatch(getDBPokemons());
+        }
+
+        if(!types.length){
+            dispatch(getTypes());
+        }
+    },[dispatch,data.records.length,types.length]);
     
     return(
         <div id="landing-container">
